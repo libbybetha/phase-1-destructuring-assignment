5a0686dba003724056635abc20b63d366c676a43
Beth Njuguna <libbybetha@gmail.com>
2022-08-24 14:18:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+
+
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ')
+
+
+
+const [blackAndWhite, , black, pink, ] = farmAnimals.split(' ')
+
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+
+
+
+const [r, o, y, g, b, i, v] = colors
+const[, , , , , indg, ] = colors
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -8,7 +26,11 @@ const muppet = {
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
+  
+ 
 };
+const { muppetName, color, song, job, partner } = muppet; 
+
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -21,9 +43,16 @@ const nestedMuppet = {
       song4: 'I Hope That Something Better Comes Along',
     },
   },
+  
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
-};
+  
+}
+const {song2,song4} = nestedMuppet.album.theMuppetMovie;
+song2;
+song4;
+const{nestedJob,nestedPartner} = nestedMuppet;
+console.log(nestedJob.nestedPartner);
 
 // Strings
 
